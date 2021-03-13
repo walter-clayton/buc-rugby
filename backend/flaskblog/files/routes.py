ed1acb8a649bb688ad4824125ba541183e9bfae7
walthedude <walter.h.clayton@gmail.com>
2021-03-13 21:26:12 +0100

    it works

--- backend/flaskblog/files/routes.py
+++ backend/flaskblog/files/routes.py
@@ -1,11 +1,15 @@
 from flask import render_template, Blueprint
+import boto3 
+
 
 files = Blueprint('files', __name__)
 
-
-@files.route("/files")
-def files():
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(S3_BUCKET)
+@files.route("/file")
+def file():
+    s3_resource = boto3.resource('s3',
+        aws_access_key_id='AKIAT2SZHIJOKKFNYTGO',
+        aws_secret_access_key='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'
+    )
+    my_bucket = s3_resource.Bucket('afitpilot-stage')
     summaries = my_bucket.objects.all()
     return render_template('files.html', my_bucket=my_bucket, files=summaries)
